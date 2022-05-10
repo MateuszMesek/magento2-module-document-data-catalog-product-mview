527dc39804f15f8f4bfa309d05b3dc2e3e42ad90
Mateusz Mesek <mateusz@mesek.pl>
2022-05-10 22:43:58 +0200

    wip

--- src/SubscriptionProvider/Attribute/Generator.php
+++ src/SubscriptionProvider/Attribute/Generator.php
@@ -5,28 +5,33 @@ namespace MateuszMesek\DocumentDataCatalogProductMview\SubscriptionProvider\Attr
 use InvalidArgumentException;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
-use Magento\Eav\Model\Config;
+use Magento\Eav\Model\Config as EavConfig;
 use Magento\Framework\DB\Ddl\Trigger;
 use Magento\Framework\EntityManager\MetadataPool;
+use Magento\Store\Model\ResourceModel\Store as StoreResource;
+use Magento\Store\Model\StoreDimensionProvider;
 use MateuszMesek\DocumentDataIndexMview\Data\SubscriptionFactory;
 use Traversable;
 
 class Generator
 {
     private MetadataPool $metadataPool;
-    private Config $config;
+    private EavConfig $eavConfig;
+    private StoreResource $storeResource;
     private SubscriptionFactory $subscriptionFactory;
     private ProductResource $productResource;
 
     public function __construct(
-        MetadataPool $metadataPool,
-        Config $config,
+        MetadataPool        $metadataPool,
+        EavConfig           $eavConfig,
+        StoreResource       $storeResource,
         SubscriptionFactory $subscriptionFactory,
-        ProductResource $productResource
+        ProductResource     $productResource
     )
     {
         $this->metadataPool = $metadataPool;
-        $this->config = $config;
+        $this->eavConfig = $eavConfig;
+        $this->storeResource = $storeResource;
         $this->subscriptionFactory = $subscriptionFactory;
         $this->productResource = $productResource;
     }
@@ -35,11 +40,14 @@ class Generator
     {
         $metadata = $this->metadataPool->getMetadata(ProductInterface::class);
 
-        $attribute = $this->config->getAttribute($metadata->getEavEntityType(), $code);
+        $attribute = $this->eavConfig->getAttribute($metadata->getEavEntityType(), $code);
 
         if (!$attribute) {
             throw new InvalidArgumentException("Attribute '$code' not found");
         }
+
+        $storeTable = $this->storeResource->getMainTable();
+        $storeDimensionName = StoreDimensionProvider::DIMENSION_NAME;
 
         foreach (Trigger::getListOfEvents() as $event) {
             switch ($event) {
@@ -56,57 +64,88 @@ class Generator
                     throw new InvalidArgumentException("Trigger event '$event' is unsupported");
             }
 
-            $condition = '';
-            $dimensions = "JSON_SET('{}', '$.store_id', 0)";
+            $condition = null;
+            $rows = <<<SQL
+                SELECT {$metadata->getIdentifierField()} AS document_id,
+                       NULL AS node_path,
+                       JSON_SET('{}', '$.$storeDimensionName', store.store_id) AS dimensions
+                FROM {$metadata->getEntityTable()}
+                CROSS JOIN $storeTable AS store
+                    ON store.store_id != 0
+                WHERE {$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()}
+            SQL;
 
             if (!$attribute->isStatic()) {
                 $condition = "$prefix.attribute_id = {$attribute->getAttributeId()}";
-                $dimensions = "JSON_SET('{}', '$.store_id', $prefix.store_id)";
+                $rows .= <<<SQL
+                    AND IF($prefix.store_id = 0, 1, store.store_id = $prefix.store_id)
+                SQL;
             }
 
             yield $this->subscriptionFactory->create([
                 'tableName' => $attribute->getBackendTable(),
                 'triggerEvent' => $event,
                 'condition' => $condition,
-                'documentId' => "(SELECT {$metadata->getIdentifierField()} FROM {$metadata->getEntityTable()} WHERE {$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()})",
-                'dimensions' => $dimensions,
+                'rows' => $rows,
             ]);
 
             $relationTable = $this->productResource->getTable('catalog_product_relation');
 
             if (in_array($relation, ['both', 'parent'])) {
+                $rows = <<<SQL
+                    SELECT parent.{$metadata->getIdentifierField()}  AS document_id,
+                       NULL AS node_path,
+                       JSON_SET('{}', '$.$storeDimensionName', store.store_id) AS dimensions
+                    FROM {$metadata->getEntityTable()} AS child
+                    INNER JOIN $relationTable AS relation
+                        ON relation.child_id = child.{$metadata->getIdentifierField()}
+                    INNER JOIN {$metadata->getEntityTable()} AS parent
+                        ON parent.{$metadata->getLinkField()} = relation.parent_id
+                    CROSS JOIN $storeTable AS store
+                        ON store.store_id != 0
+                    WHERE child.{$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()}
+                SQL;
+
+                if (!$attribute->isStatic()) {
+                    $rows .= <<<SQL
+                        AND IF($prefix.store_id = 0, 1, store.store_id = $prefix.store_id)
+                    SQL;
+                }
+
                 yield $this->subscriptionFactory->create([
                     'tableName' => $attribute->getBackendTable(),
                     'triggerEvent' => $event,
                     'condition' => $condition,
-                    'documentId' => <<<SQL
-                        (SELECT parent.{$metadata->getIdentifierField()}
-                         FROM {$metadata->getEntityTable()} AS child
-                         INNER JOIN $relationTable AS relation
-                            ON relation.child_id = child.{$metadata->getIdentifierField()}
-                         INNER JOIN {$metadata->getEntityTable()} AS parent
-                            ON parent.{$metadata->getLinkField()} = relation.parent_id
-                         WHERE child.{$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()})
-                    SQL,
-                    'dimensions' => $dimensions,
+                    'rows' => $rows
                 ]);
             }
 
             if (in_array($relation, ['both', 'child'])) {
+                $rows = <<<SQL
+                    SELECT child.{$metadata->getIdentifierField()} AS document_id,
+                       NULL AS node_path,
+                       JSON_SET('{}', '$.$storeDimensionName', store.store_id) AS dimensions
+                    FROM {$metadata->getEntityTable()} AS parent
+                    INNER JOIN $relationTable AS relation
+                        ON relation.parent_id = parent.{$metadata->getLinkField()}
+                    INNER JOIN {$metadata->getEntityTable()} AS child
+                        ON child.{$metadata->getIdentifierField()} = relation.child_id
+                    CROSS JOIN $storeTable AS store
+                        ON store.store_id != 0
+                    WHERE parent.{$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()}
+                SQL;
+
+                if (!$attribute->isStatic()) {
+                    $rows .= <<<SQL
+                        AND IF($prefix.store_id = 0, 1, store.store_id = $prefix.store_id)
+                    SQL;
+                }
+
                 yield $this->subscriptionFactory->create([
                     'tableName' => $attribute->getBackendTable(),
                     'triggerEvent' => $event,
                     'condition' => $condition,
-                    'documentId' => <<<SQL
-                        (SELECT child.{$metadata->getIdentifierField()}
-                         FROM {$metadata->getEntityTable()} AS parent
-                         INNER JOIN $relationTable AS relation
-                            ON relation.parent_id = parent.{$metadata->getLinkField()}
-                         INNER JOIN {$metadata->getEntityTable()} AS child
-                            ON child.{$metadata->getIdentifierField()} = relation.child_id
-                         WHERE parent.{$metadata->getLinkField()} = $prefix.{$metadata->getLinkField()})
-                    SQL,
-                    'dimensions' => $dimensions,
+                    'rows' => $rows
                 ]);
             }
         }
